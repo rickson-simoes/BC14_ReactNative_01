76f9294c624b22b4bb82bf76fc77e32743e24d23
Rickson <rickson.simoes@hotmail.com>
2020-06-28 11:29:13 -0300

    :hammer: Eslint - Bug

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import Input from '../../components/Input';
-import Button from '../../components/Button';
+import Input from '../../components/input';
+import Button from '../../components/button';
 
 import logoImg from '../../assets/logo.png';
 
